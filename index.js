7e0a3b53f2510cd89760aca80d78c70abdfb1fb2
Yusra <yusra7003@gmail.com>
2024-04-08 00:30:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    SAR: 73.99,
+    JPY: 1.83,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR", "JPY"],
+    },
+    {
+        name: 'to',
+        message: "Enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR", "JPY"],
+    },
+    {
+        name: "amount",
+        message: "Enter your amount ",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let basedAmount = amount / fromAmount; //USD base currency 
+let convertedAmount = basedAmount * toAmount;
+console.log(convertedAmount);
